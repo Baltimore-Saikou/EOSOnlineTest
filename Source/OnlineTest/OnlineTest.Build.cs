9d74d2813102eb5cc7253314f1a3cf69eccc1fc0
Baltimore <BaltimoreSaikou@gmail.com>
2022-04-24 00:02:01 +0800

    添加GAS和EnhancedInput插件

--- Source/OnlineTest/OnlineTest.Build.cs
+++ Source/OnlineTest/OnlineTest.Build.cs
@@ -8,6 +8,24 @@ public class OnlineTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara" });
+        PublicDependencyModuleNames.AddRange(
+			new string[] { 
+				"Core", 
+				"CoreUObject", 
+				"Engine", 
+				"InputCore", 
+				"HeadMountedDisplay", 
+				"NavigationSystem", 
+				"AIModule", 
+				"Niagara", 
+				"GameplayAbilities" 
+			}
+		);
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"EnhancedInput",
+			}
+		);
     }
 }
